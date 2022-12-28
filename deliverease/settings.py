1bff437682525923f66fce4137d49ea396819e2a
Mohammed Anas <mohammedanasa@outlook.com>
2022-12-28 19:21:09 +0000

    Commit Authentication

--- deliverease/settings.py
+++ deliverease/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'bootstrap5',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+LOGIN_URL = '/sign-in'
+LOGIN_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
